4586c0c94252aedd99096135d1341b5ddb40aa47
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-08-06 16:46:18 -0300

    Create aula2_3.java

---
+++ java/aula2_3.java
@@ -0,0 +1,30 @@
+package com.mycompany.aula2;
+
+import java.util.Scanner;
+
+public class aula2_3 
+{
+
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int[] number = new int[2];
+
+        for (int c = 0; c < 10; c++) 
+        {
+            System.out.print("digite um numero: ");
+            number[c] = input.nextInt();
+        }
+
+        input.close();
+        
+        if (number[0] % number[1] % 2 == 0 || number[1] % number[0] % 2 == 0) 
+        {
+            System.out.println("\n\n\n" + number[0] + " e " + number[1] + " sao multiplos");
+        } 
+        else 
+        {
+            System.out.println("\n\n\n" + number[0] + " e " + number[1] + " nao sao multiplos");
+        }
+    }
+}
