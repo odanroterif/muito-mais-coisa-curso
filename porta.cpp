ba302af9572fe487bb1f52115dbc9186a03e8618
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-09-11 15:57:57 -0300

    Create porta.cpp

---
+++ porta.cpp
@@ -0,0 +1,130 @@
+// C++ code
+//
+int green_led = 2;
+int white_led = 3;
+int second_green_led = 4;
+int piezo = 5;
+int blue_led = 6;
+
+void setup()
+{
+  pinMode(green_led, OUTPUT);
+  pinMode(white_led, OUTPUT);
+  pinMode(second_green_led, OUTPUT);
+  pinMode(piezo, OUTPUT);
+  pinMode(blue_led, OUTPUT);
+  Serial.begin(8000);
+}
+
+void loop()
+{
+  if(Serial.read() == 'a')
+  {
+    digitalWrite(green_led,HIGH);
+    digitalWrite(second_green_led,LOW);
+    digitalWrite(white_led, LOW);
+    digitalWrite(piezo,LOW);
+  }
+  else if(Serial.read() == 'b')
+  {
+    digitalWrite(green_led,LOW);
+    digitalWrite(second_green_led,LOW);
+    digitalWrite(white_led, HIGH);
+    digitalWrite(piezo,LOW);
+  }   
+  else if(Serial.read() == 'c')
+  {
+    digitalWrite(green_led,LOW);
+    digitalWrite(second_green_led,HIGH);
+    digitalWrite(white_led, LOW);  
+    digitalWrite(piezo,LOW);
+  } 
+  else if(Serial.read() == 'd')
+  {
+    digitalWrite(green_led,HIGH);
+    digitalWrite(second_green_led,HIGH);
+    digitalWrite(white_led, HIGH);  
+    digitalWrite(piezo,HIGH);
+    delay(150);
+    digitalWrite(piezo,LOW);
+    delay(150);
+    digitalWrite(piezo,HIGH);
+    delay(150);
+    digitalWrite(piezo,LOW);
+    delay(150);
+    digitalWrite(piezo,HIGH);
+    delay(150);
+    digitalWrite(piezo,LOW);
+    delay(150);
+    digitalWrite(piezo,HIGH);
+  } 
+  else if(Serial.read() == 'L')
+  {
+    sing_leds();
+  }
+  //only piezo
+  if(Serial.read() == 'q')
+  {
+    digitalWrite(green_led,LOW);
+    digitalWrite(second_green_led,LOW);
+    digitalWrite(white_led, LOW);      
+    digitalWrite(blue_led, LOW);
+    digitalWrite(piezo, HIGH);
+  }
+  else if(Serial.read() == 'Q')
+  {
+    digitalWrite(piezo, LOW);
+  }    
+  // only blue led
+  if(Serial.read() == 'w')
+  {
+    digitalWrite(green_led,LOW);
+    digitalWrite(second_green_led,LOW);
+    digitalWrite(white_led, LOW);  
+    digitalWrite(piezo,LOW);
+    digitalWrite(blue_led, HIGH);
+  }
+  else if(Serial.read() == 'W')
+  {
+    digitalWrite(blue_led, LOW);
+  } 
+}
+
+
+
+void sing_leds()
+{  
+  digitalWrite(white_led,LOW);
+  digitalWrite(second_green_led,LOW);
+  digitalWrite(green_led,HIGH);
+  delay(200);
+  digitalWrite(green_led,LOW);
+  digitalWrite(white_led,HIGH);
+  delay(200);  
+  digitalWrite(white_led,LOW);
+  digitalWrite(second_green_led,HIGH);
+  delay(200);
+  digitalWrite(second_green_led,LOW);
+  digitalWrite(white_led,HIGH);
+  delay(200);
+  digitalWrite(white_led,LOW);
+  delay(200);
+  digitalWrite(white_led,HIGH);
+  delay(200);
+  digitalWrite(white_led,LOW);
+  delay(200);
+  digitalWrite(white_led,HIGH);
+  digitalWrite(green_led,HIGH);
+  digitalWrite(second_green_led,HIGH);
+  delay(1000);
+  digitalWrite(white_led,LOW);
+  digitalWrite(green_led,LOW);
+  digitalWrite(second_green_led,LOW);
+  delay(500);
+  digitalWrite(green_led,HIGH);  
+  digitalWrite(second_green_led,HIGH);
+  delay(300);
+  digitalWrite(green_led,LOW);
+  digitalWrite(second_green_led,LOW);
+  digitalWrite(white_led,HIGH);
+}
