28da797eaefaa4037c8ec64263bf59eef67002a2
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-09-17 13:42:12 -0300

    Update bancoemprestimo.java

--- java/bancoemprestimo.java
+++ java/bancoemprestimo.java
@@ -1,87 +1,3 @@
-package operations;
-
-public class bank 
-{
-    int number_account;
-    String person;
-    double total;
-    int[] limit = new int[2];  
-
-    public bank() 
-    {
-
-    }
-
-    public bank(int number_account, double total, String person)
-    {
-        this.person = person;
-        this.number_account = number_account;
-        this.total = total;
-    }
-    
-    public bank(int number_account, double total, String person,double loan)
-    {
-        this.person = person;
-        this.number_account = number_account;
-        this.total = total;
-    }
-
-    public bank(int number_account, String person)
-    {
-        this.person = person;
-        this.number_account = number_account;
-    }
-    
-    public int account()
-    {
-        return number_account;
-    }
-    
-    public String people()    
-    {
-        return person;
-    }
-    
-    public void alter_people(String person)    
-    {
-        this.person = person;
-    }
-    
-    public void sake(double money) 
-    {
-        this.total -= money;
-    }
-
-    public void deposit(double money) 
-    {
-        this.total += money + 5;
-    }
-    
-    public int limit(double money) 
-    {
-        return this.limit[0]++;
-    }
-    
-    public double loan_limit(double money) 
-    {
-        if (money > 1000) 
-        {
-            money = 1000;
-        }
-        return money - 10;
-    }
-      
-    public double alter_total()
-    {
-        return total;
-    }
-    
-    public  String tString()
-    {
-       return "Conta: " + number_account + " titular "+ person+ " saldo: R$"+ String.format("%.2f",total);
-    }
-}
-//-------------------------------------------------------
 package com.mycompany.banco;
 
 import java.util.Scanner;
@@ -89,19 +5,23 @@ import cleanscreen.clean;
 import java.awt.AWTException;
 import java.io.IOException;
 import operations.bank;
+import operations.special_bank;
 
-public class Banco {
+public class Banco 
+{
 
     public static void main(String[] args) throws IOException, InterruptedException, AWTException {
         Scanner input = new Scanner(System.in);
         bank object = new bank();
         clean clear = new clean();
-        double money;
+        special_bank special_object = new special_bank();
+        double money = 0;
         String name;
         int account;
         
         System.out.println("É conta empresarial?");
-        String resward = input.next();
+        String resward = input.nextLine();
+        clear.clear();
 
         if ("sim".equals(resward)) 
         {            
@@ -111,29 +31,49 @@ public class Banco {
             System.out.print("o numero da conta: ");
             account = input.nextInt();
             clear.clear();
-            System.out.println("Deseja reailizar saque,deposito ou emprestimo?");
+            System.out.println("quer realizar um deposito inicial?");
             resward = input.next();
-            switch (resward) 
+            clear.clear();
+            if ("sim".equals(resward))
             {
-                case "saque":
-                    System.out.println("quanto?");
-                    money = input.nextDouble();
-                    object.sake(money);
-                    break;
-                case "deposito":
-                    System.out.println("quanto?");
-                    money = input.nextDouble();
-                    object.deposit(money);
-                    break;
-                case "emprestimo":
-                    System.out.println("quanto?");
-                    money = input.nextDouble();
-                    object.limit(money);
-                    
-                    break;
-                default:
-                    throw new AssertionError();
+                System.out.println("quanto?");
+                money = input.nextDouble();
+                special_object = new special_bank(account, money, name);
+            } 
+            else 
+            {
+                special_object = new special_bank(account, name);
             }
+            clear.clear();
+            do 
+            {                
+               System.out.println("Deseja reailizar saque,deposito ou emprestimo?");
+               resward = input.next();
+               clear.clear();
+               switch (resward) 
+               {
+                 case "saque":
+                    System.out.println("quanto?");
+                    money = input.nextDouble();
+                    special_object.sake(money);
+                    break;
+                 case "deposito":
+                    System.out.println("quanto?");
+                    money = input.nextDouble();
+                    special_object.deposit(money);
+                    break;
+                 case "emprestimo":
+                    System.out.println("quanto?");
+                    money = input.nextDouble();
+                    special_object.limit(money);                    
+                    break;
+                 default:
+                    System.out.println("não fazemos esta operação");
+               } 
+                System.out.println("continuar operações?");
+                resward = input.next();
+            } while ("sim".equals(resward));
+                  
         }
         else
         {
@@ -180,7 +120,20 @@ public class Banco {
             } while ("sim".equals(resward));
             clear.clear();
         }
-        System.out.println(object.tString());
+        clear.clear();
+        System.out.println("consultar conta de pessoa fisica ou empresa?");
+        resward = input.next();    
+        if ("pessoa".equals(resward)) 
+        {
+            System.out.println(object.tString());
+        }
+        else if("empresa".equals(resward))
+        {
+            System.out.println(special_object.tString());
+        }
+        else
+        {
+            System.out.println("essa conta o lula levou");
+        }
     }
 }
-//----------------------------------------------------
