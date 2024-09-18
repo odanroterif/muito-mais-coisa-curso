0bbc8266d97be9284d4d18a19762b43a6193c8cb
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-09-18 19:04:28 +0000

    Create projeto.cpp

---
+++ projeto.cpp
@@ -0,0 +1,21 @@
+// C++ code
+//
+int piezo;
+int piezo_two;
+int red_led;
+int second_red_led;
+int white_led;
+int fotoresistor;
+int button;
+int pir_sensor;
+    
+  
+void setup()
+{
+
+}
+
+void loop()
+{
+  delay(10); // Delay a little bit to improve simulation performance
+}
