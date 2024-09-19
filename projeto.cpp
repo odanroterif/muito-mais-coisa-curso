bf83c8490a6c773951999076deac2bf3ba440f19
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-09-19 17:36:18 -0300

    Update projeto.cpp

--- projeto.cpp
+++ projeto.cpp
@@ -1,8 +1,7 @@
+#include <Keypad.h>
+
 // C++ code
 //
-
-
-
 int piezo = 2;
 int piezo_two = 3;
 int red_led = 4;
@@ -15,17 +14,19 @@ int pir_sensor = 9;
 int pir_sensor_state = 0;
 int potenciomentro_position = A1;
 int potenciometro_value = 0;
+int button_state = 0;
 //keypad variables-------------
-const int line = 4;
-const int column = 4;
-int line_pins[line] = {7,10,11,12};
-int column_pins[column] = {A5,A4,A3,13};
-char keypad[line][column] = {
+const byte line = 4;
+const byte column = 4;
+byte line_pins[line] = {7,10,11,12};
+byte column_pins[column] = {A5,A4,A3,13};
+char keipad[line][column] = {
   {'0','1','2','3'},
   {'4','5','6','7'},
   {'8','9','A','B'},
   {'C','D','E','F'}
 };
+Keypad keypad = Keypad(makeKeymap(keipad),line_pins,column_pins,line,column); 
 //-----------------------------
     
   
@@ -35,16 +36,34 @@ void setup()
   pinMode(second_red_led,OUTPUT);
   pinMode(piezo, OUTPUT);
   pinMode(white_led,OUTPUT);
-  pinMode(piezo_two_led,OUTPUT);
+  pinMode(piezo_two,OUTPUT);
   pinMode(piezo,OUTPUT);    
-  pinMode(button,INTPUT);
-  pinMode(pir_sensor,INTPUT);
+  pinMode(button,INPUT);
+  pinMode(pir_sensor,INPUT);
   Serial.begin(9600);
 }
 
 void loop()
 {
+  button_state = digitalRead(button);
   sensor_value = analogRead(light_sensor);
   potenciometro_value = analogRead(potenciomentro_position);
   pir_sensor_state = analogRead(pir_sensor);
+  int condition = digitalRead(white_led);
+  String password = "2468";
+  
+  Serial.println(potenciometro_value);
+  //botão
+  if(button_state == HIGH && condition == 0)
+  {    
+    digitalWrite(white_led,HIGH);
+  }  
+  else if (button_state == HIGH && condition == 1)
+  {    
+    digitalWrite(white_led,LOW);
+  }
+  //potenciomentro
+  if(potenciometro_value >= 400 && potenciometro_value <= 500)
+  {
+  }
 }
